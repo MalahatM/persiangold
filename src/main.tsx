c4a8e7e9131ad6131a7aa5d175d0dd7e1b41e7f4
your_username <malahat.m82@gmail.com>
2026-02-24 11:59:36 +0100

    Refine mobile side menu active styles (gold highlight, remove white border)

--- src/main.tsx
+++ src/main.tsx
@@ -11,12 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
